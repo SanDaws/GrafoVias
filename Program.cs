0e4f91820feddd93e9366d50e5877ac79bf248a4
Daws <castrogiraldosantiago@gmail.com>
2025-03-03 10:05:51 -0500

    test de ciudaddes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Grafovias;
+﻿
+using Grafovias.Data;
+
+namespace Grafovias;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        List<string> modeus=FileIO.UploadFile($@"Data\Datos vias.csv");
+        HashSet<string> ciudades=new HashSet<string>();
+        foreach (string item in modeus)
+        {
+            string[] divi= item.Split(";");
+            ciudades.Add(divi[0]);
+            ciudades.Add(divi[1]);
+        }
+
+        foreach (string recorrido in ciudades)
+        {
+            Console.WriteLine(recorrido);
+            
+        }
     }
 }
